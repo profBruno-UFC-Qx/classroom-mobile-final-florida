1dd102233923bc462f4ccb1c87bddba0dd22db79
Heric da Silva Cruz <hericsilvaho@gmail.com>
2026-06-22 13:32:44 -0300

    adiciona configuracao do banco

---
+++ florida-api/database.py
@@ -0,0 +1,18 @@
+from collections.abc import Generator
+from sqlmodel import Session, SQLModel, create_engine
+
+DATABASE_URL = "sqlite:///florida.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False},
+)
+
+
+def create_florida_db() -> None:
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session() -> Generator[Session]:
+    with Session(engine) as session:
+        yield session
